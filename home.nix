f0b2201f89cc0dddae5ddeaa16a659cf7e1a7966
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-08-19 14:15:02 -0400

    20   2024-08-19 14:13:13   (current)

--- home.nix
+++ home.nix
@@ -17,7 +17,8 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
+    hello
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,7 @@ git diff -U0 *.nix
 echo "Rebuilding nix"
 mkdir -p logs
 darwin-rebuild switch --flake . &>logs/darwin-rebuild.log || (
-    cat darwin-rebuild.log | grep --color error && false)
+    cat logs/darwin-rebuild.log | grep --color error && false)
 gen=$(darwin-rebuild --list-generations | grep current)
 git commit -am "$gen"
 popd 
