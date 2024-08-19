d1dcf97eee528550b32f6b85068dec96aa2d0618
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-08-19 13:43:42 -0400

    18   2024-08-19 13:43:35   (current)

--- rebuild.sh
+++ rebuild.sh
@@ -4,7 +4,8 @@ pushd ~/.config/nix
 alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "Rebuilding nix"
-darwin-rebuild switch --flake . &>darwin-rebuild.log || (
+mkdir -p logs
+darwin-rebuild switch --flake . &>logs/darwin-rebuild.log || (
     cat darwin-rebuild.log | grep --color error && false)
 gen=$(darwin-rebuild --list-generations | grep current)
 git commit -am "$gen"
