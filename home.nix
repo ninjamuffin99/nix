1a7769e13228d904d7783cb1bce3cd6acf00461a
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-08-19 14:00:47 -0400

    19   2024-08-19 14:00:42   (current)

--- home.nix
+++ home.nix
@@ -18,10 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -67,4 +63,10 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+  };
 }
