e4d6fce16bca726a1cf5a0fbada8b58fad01acfb
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-09-04 22:11:22 -0400

    move sum files

--- nixos-configuration.nix
+++ nixos-configuration.nix
@@ -102,6 +102,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -119,6 +121,7 @@
     nix-search-cli
     spotify
     lshw
+    wezterm
   ];
 
 
