75e07367dc16189d1b358682dba006730061b662
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-02-12 12:44:20 -0500

    roll back to release 24.11 versions

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -3,12 +3,12 @@
   description = "Darwin config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     alejandra.url = "github:kamadorueda/alejandra/3.1.0";
